1be4e376438cb0001bfddcc3523104e6f7369f96
SimonBarrettACT <simonbarrett@acttraining.org.uk>
2020-03-30 11:04:31 +0100

    Installed GitFox

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core"
 @Component({
   selector: 'event-thumbnail',
   template: `
- <div class="well hoverwell thumbnail">
+ <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
         <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
@@ -21,7 +21,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core"
         <div *ngIf="event?.onlineUrl">
           Online URL: {{event?.onlineUrl}}
         </div>
-    </div> 
+    </div>
   `,
   styles: [`
     .green { color: #003300 !important; }
