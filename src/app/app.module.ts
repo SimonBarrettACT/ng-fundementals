4649cbd6804960f0b4ffad3fda52a352dc8d484f
SimonBarrettACT <simonbarrett@acttraining.org.uk>
2020-04-06 15:53:51 +0100

    Module 10 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import {
   EventRouteActivator,
   EventListResolver,
   CreateSessionComponent,
-  SessionListComponent
+  SessionListComponent,
+  DurationPipe
 } from './events/index'
 
 import { NavBarComponent } from './nav/nav-bar.component';
@@ -41,7 +42,8 @@ import { AuthService } from "./user/auth.service";
     Error404Component,
     CreateSessionComponent,
     SessionListComponent,
-    CollapsibleWellComponent
+    CollapsibleWellComponent,
+    DurationPipe
   ],
   providers: [
     EventService,
