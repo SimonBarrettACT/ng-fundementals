92b745808946939826f434cd0a4ef8ab2452d837
SimonBarrettACT <simonbarrett@acttraining.org.uk>
2020-04-06 10:42:22 +0100

    End of Module 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import {
   CreateEventComponent,
   EventRouteActivator,
   EventListResolver,
-  CreateSessionComponent
+  CreateSessionComponent,
+  SessionListComponent
 } from './events/index'
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/nav-bar.component';
@@ -36,7 +37,8 @@ import { AuthService } from "./user/auth.service";
     EventThumbnailComponent,
     CreateEventComponent,
     Error404Component,
-    CreateSessionComponent
+    CreateSessionComponent,
+    SessionListComponent
   ],
   providers: [
     EventService,
